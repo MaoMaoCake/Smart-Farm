167a8f2740257a5990444f4c344a88439f7c1a23
maomao <tangpansak@gmail.com>
2023-01-18 20:28:52 +0700

    Added custom dark theme

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -5,4 +5,29 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+  themes: [
+    {
+      sf_dark: {
+         "primary": "#38540A",
+
+         "secondary": "#619230",
+
+         "accent": "#1FB2A6",
+
+         "neutral": "#D9D9D9",
+
+         "base-100": "#1E1E1E",
+
+         "info": "#3ABFF8",
+
+         "success": "#36D399",
+
+         "warning": "#FBBD23",
+
+         "error": "#F87272",
+      },
+    },
+  ],
+},
 }
