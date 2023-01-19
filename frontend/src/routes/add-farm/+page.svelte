560a61095421ae4c2a023576f18035c151dc8075
maomaocake <tangpansak@gmail.com>
2023-01-19 11:53:31 +0700

    add farm page

---
+++ frontend/src/routes/add-farm/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    let farm_key = "";
+    function register_fk(){
+        alert(`fk: ${farm_key}`);
+    }
+</script>
+<div class="flex align-center justify-center">
+  <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <div class="card-body">
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Enter The Key of the farm</span>
+          </label>
+          <input type="text" class="input input-bordered" bind:value={farm_key}/>
+        </div>
+        <div class="form-control mt-5">
+          <button class="btn btn-secondary white" on:click={register_fk}>Enter</button>
+        </div>
+        <div class="form-control mt-5">
+            <a href="/" class="btn btn-primary white">Cancel</a>
+        </div>
+      </div>
+  </div>
+</div>
+<style>
+    .white {
+        color: white
+    }
+</style>
