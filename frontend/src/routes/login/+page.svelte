c0c4f497e30c84d893d8c1c59db8d20e9fac02e2
maomaocake <tangpansak@gmail.com>
2023-01-19 11:25:11 +0700

    added login prompt

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,4 +1,29 @@
+<script lang="ts">
+  let username = "";
+  let password = "";
 
-<button class="btn btn-primary text-neutral">Hello</button>
-<button class="btn btn-secondary text-neutral">Hello</button>
-
+</script>
+<div class="flex align-center justify-center">
+  <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <div class="card-body">
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Username</span>
+          </label>
+          <input type="text" placeholder="username" class="input input-bordered" bind:value={username}/>
+        </div>
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Password</span>
+          </label>
+          <input type="text" placeholder="password" class="input input-bordered" bind:value={password}/>
+          <label class="label">
+            <a href="forgot-password" class="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div class="form-control mt-6">
+          <button class="btn btn-primary">Login</button>
+        </div>
+      </div>
+</div>
+</div>
