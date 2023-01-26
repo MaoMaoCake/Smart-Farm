e3015aef34525b838b6833bad4fe7627758d2018
maomao <tangpansak@gmail.com>
2023-01-26 21:15:31 +0700

    margins in add farm

--- frontend/src/lib/AddFarm.svelte
+++ frontend/src/lib/AddFarm.svelte
@@ -4,6 +4,6 @@
 </script>
 <div class="flex flex-col justify-center p-10">
   <Icon icon="mdi:box-add" class="w-52 h-52 pl-10"/>
-  <p>No Farms Found, add your first one!</p>
+  <p class="mt-10 mb-10">No Farms Found, add your first one!</p>
   <a href="/add-farm" class="btn btn-primary">Add Farm</a>
 </div>
