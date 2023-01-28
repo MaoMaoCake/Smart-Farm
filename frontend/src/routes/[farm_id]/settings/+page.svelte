93b0b286fdfbd48d7ade6819a6eff00ece3db092
maomaocake <tangpansak@gmail.com>
2023-01-28 10:40:19 +0700

    settings page example load data

--- frontend/src/routes/[farm_id]/settings/+page.svelte
+++ frontend/src/routes/[farm_id]/settings/+page.svelte
@@ -1,1 +1,7 @@
-settings
+<script lang="ts">
+  import type { PageData } from './$types';
+
+  export let data: PageData;
+</script>
+
+<p>This is the settings for {data.farm_id}</p>
---
+++ frontend/src/routes/[farm_id]/settings/+page.ts
@@ -0,0 +1,5 @@
+export const load = (({ params }: any) => {
+  return {
+    farm_id: params.farm_id
+  };
+})
