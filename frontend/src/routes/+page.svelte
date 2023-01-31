fb84d5a26359ea0debf215bcb65d081604b995b6
maomaocake <tangpansak@gmail.com>
2023-01-31 15:55:35 +0700

    moved interface into page from type

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,20 @@
-<script>
+<script lang="ts">
     import StatPreview from "$lib/StatPreview.svelte";
     import AddFarm from "$lib/AddFarm.svelte";
-    let data = [
+    //import { FarmData } from "$lib/SF_types.d.ts"
+
+    interface FarmData {
+        name: string,
+        temp: number,
+        humidity: number,
+        light: boolean,
+        ac: boolean,
+        humidifier: boolean,
+        co2_val: number,
+        co2: boolean
+    }
+
+    let data: FarmData[] = [
         {name:"farm 1", temp: 25, humidity: 2, light: true, ac: false, humidifier: true, co2_val: 900, co2: true},
         {name:"farm 2", temp: 24, humidity: 5, light: false, ac: true, humidifier: false, co2_val: 1000, co2: false},
         {name:"farm 3", temp: 16, humidity: 5, light: false, ac: true, humidifier: false, co2_val: 800, co2: false},
