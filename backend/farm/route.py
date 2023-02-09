f8f05a5851602e9a00ee3c149b373457e1702ad7
KagErou710 <56529285+KagErou710@users.noreply.github.com>
2023-02-09 15:12:27 +0700

    [#52] feature/api-list-light-in-preset

--- backend/farm/route.py
+++ backend/farm/route.py
@@ -5,9 +5,9 @@ from auth.utils import get_current_active_user
 from response.response_dto import ResponseDto
 
 from auth.models import User
-from .utils import link_farm_to_user, list_farms, list_light
+from .utils import link_farm_to_user, list_farms, list_light, get_light_in_preset
 
-from .models import FarmOwner, FarmStats, Light
+from .models import FarmOwner, FarmStats, Light, LightCombination
 
 farmRouter = APIRouter()
 
@@ -28,5 +28,20 @@ async def list_all_farms(current_user: User = Depends(get_current_active_user)):
 async def list_all_lights(farm_id: int, current_user: User = Depends(get_current_active_user)):
 
     return list_light(farm_id, current_user.username)
+
+
+@farmRouter.get("/farm/{farm_id}/light/preset/{preset_id}", response_model=ResponseDto[LightCombination], tags=["Farm"])
+async def show_light_from_preset(farm_id: int, preset_id: int, current_user: User = Depends(get_current_active_user)):
+
+    return get_light_in_preset(farm_id, preset_id, current_user.username)
+
+
+# @farmRouter.get("/farm/{farm_id}/light/preset", response_model=ResponseDto[FarmLightPreset], tags=["Farm"])
+# async def list_all_light_preset(farm_id: int, current_user: User = Depends(get_current_active_user)):
+# #? get preset one by one?
+#     return list_light(farm_id, current_user.username)
+
+
+
 
 
