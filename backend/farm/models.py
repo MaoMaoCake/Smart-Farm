d9b0b9b6dbccee12b6886cbac3cd647427541d44
KagErou710 <56529285+KagErou710@users.noreply.github.com>
2023-02-14 22:09:44 +0700

    [#54]feature/api-get-farm-setting

--- backend/farm/models.py
+++ backend/farm/models.py
@@ -1,5 +1,8 @@
+from datetime import datetime, time
+
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
+
 
 class FarmOwner(BaseModel):
     id: int
@@ -19,26 +22,26 @@ class FarmStats(BaseModel):
     CO2controllerStatus: bool
 
     def __init__(self,
-                farm_id: int,
-                farm_name: str,
-                temperature: int,
-                ac_status: bool,
-                humidity_level: int,
-                dehumidifier_status: bool,
-                light_status: bool,
-                co2_level: int,
-                co2_controller_status: bool
-                ):
+                 farm_id: int,
+                 farm_name: str,
+                 temperature: int,
+                 ac_status: bool,
+                 humidity_level: int,
+                 dehumidifier_status: bool,
+                 light_status: bool,
+                 co2_level: int,
+                 co2_controller_status: bool
+                 ):
         super().__init__(
-        farmId= farm_id,
-        farmName = farm_name,
-        temperature = temperature,
-        ACStatus = ac_status,
-        humidityLevel = humidity_level,
-        dehumidifierStatus = dehumidifier_status,
-        lightStatus = light_status,
-        CO2Level = co2_level,
-        CO2controllerStatus = co2_controller_status
+            farmId=farm_id,
+            farmName=farm_name,
+            temperature=temperature,
+            ACStatus=ac_status,
+            humidityLevel=humidity_level,
+            dehumidifierStatus=dehumidifier_status,
+            lightStatus=light_status,
+            CO2Level=co2_level,
+            CO2controllerStatus=co2_controller_status
         )
 
 
@@ -61,12 +64,12 @@ class LightCombination(BaseModel):
 
     def __init__(self,
                  light_name: str,
-                preset_id: int,
-                light_id: int,
-                natural_lightDensity: int,
-                UV_lightDensity: int,
-                IR_lightDensity: int
-                ):
+                 preset_id: int,
+                 light_id: int,
+                 natural_lightDensity: int,
+                 UV_lightDensity: int,
+                 IR_lightDensity: int
+                 ):
         super().__init__(
             lightName=light_name,
             presetId=preset_id,
@@ -96,6 +99,7 @@ class AC(BaseModel):
     ACName: str
     ACStatus: str
 
+
 class CreateLightInput(BaseModel):
     name: Optional[str]
     automation: Optional[bool]
@@ -109,4 +113,49 @@ class UpdateLightStrengthInput(BaseModel):
     NaturalLightDensity: int
     UVLightDensity: int
     IRLightDensity: int
-    
+
+
+class LightAutomation(BaseModel):
+    lightAutomationId: int
+    startTime: time
+    endTime: time
+    farmLightPresetId: int
+
+
+class ACAutomation(BaseModel):
+    ACAutomationId: int
+    startTime: time
+    endTime: time
+    temperature: int
+
+
+class WateringAutomation(BaseModel):
+    wateringAutomationId: int
+    wateringStartTime: time
+    wateringEndTime: time
+
+
+class GetFarmSettings(BaseModel):
+    MinCO2Level: int
+    MaxHumidityLevel: int
+    LightAutomations: List[LightAutomation]
+    FarmLightPresets: List[FarmLightPreset]
+    ACAutomations: List[ACAutomation]
+    WateringAutomations: List[WateringAutomation]
+
+    def __int__(self,
+                min_co2_level: int,
+                max_humidity: int,
+                light_automations: List[LightAutomation],
+                farm_light_presets: List[FarmLightPreset],
+                ac_automations: List[ACAutomation],
+                watering_automations: List[WateringAutomation]
+                ):
+        super().__init__(
+            MinCO2Level=min_co2_level,
+            MaxHumidityLevel=max_humidity,
+            LightAutomations=light_automations,
+            FarmLightPresets=farm_light_presets,
+            ACAutomations=ac_automations,
+            WateringAutomations=watering_automations
+        )
