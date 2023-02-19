cfccdda4675c29d108a6cc90b95715528b3db26e
maomao <tangpansak@gmail.com>
2023-02-19 21:08:34 +0700

    added stats preview large to settings page

--- frontend/src/routes/[farm_id]/settings/+page.svelte
+++ frontend/src/routes/[farm_id]/settings/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { PageData } from './$types';
   import StatPreview from "$lib/StatPreview.svelte";
+  import StatPreviewLarge from "$lib/StatPreviewLarge.svelte";
   import Icon from "@iconify/svelte";
   import {beforeUpdate, onMount} from "svelte";
   import { invalidateAll } from "$app/navigation"
@@ -50,39 +51,45 @@
 </script>
 
 <div class="flex w-full justify-center items-center flex-col md:flex-row">
-    <div class="flex flex-col grow">
-        <div>
-        <StatPreview farm_name="{data.farm_data.name}" temp="{data.farm_data.temp}" humidity={data.farm_data.humidity}
-                     light={data.farm_data.light} ac="{data.farm_data.ac}" humidifier={data.farm_data.humidifier}
-                     co2={data.farm_data.co2} co2_val={data.farm_data.co2_val}>
-        </StatPreview>
-    </div>
-        <div class="flex justify-evenly w-full">
-        <div class="flex justify-center grow items-center white">
-            {#if light_switch}
-                <Icon icon="iconoir:light-bulb-on" class="h-9 w-9 bg-amber-500 p-2 mr-1 rounded-lg"/>
-            {:else}
-                <Icon icon="iconoir:light-bulb-off" class="h-9 w-9 bg-amber-500 p-2 mr-1 rounded-lg"/>
-            {/if}
-            <input type="checkbox" class="toggle toggle-success" bind:checked={light_switch} on:click={() => {toggle_switch("light", data.farm_id, light_switch)}}/>
+    <div class="flex flex-col grow md:w-1/3 md:h-5/6 items-center justify-center">
+        <div class="flex grow md:hidden">
+            <StatPreview farm_name="{data.farm_data.name}" temp="{data.farm_data.temp}" humidity={data.farm_data.humidity}
+                         light={data.farm_data.light} ac="{data.farm_data.ac}" humidifier={data.farm_data.humidifier}
+                         co2={data.farm_data.co2} co2_val={data.farm_data.co2_val}>
+            </StatPreview>
         </div>
-        <div class="flex justify-center grow items-center white">
-            {#if ac_switch}
-                <Icon icon="tabler:air-conditioning" class="h-9 w-9 bg-blue-900 p-2 mr-1 rounded-lg"/>
-            {:else}
-                <Icon icon="tabler:air-conditioning-disabled" class="h-9 w-9 bg-blue-900 p-2 mr-1 rounded-lg"/>
-            {/if}
-            <input type="checkbox" class="toggle toggle-success" bind:checked={ac_switch} on:click={() => {toggle_switch("ac", data.farm_id, ac_switch)}}/>
+        <div class="hidden md:flex">
+            <StatPreviewLarge farm_name="{data.farm_data.name}" temp="{data.farm_data.temp}" humidity={data.farm_data.humidity}
+                         light={data.farm_data.light} ac="{data.farm_data.ac}" humidifier={data.farm_data.humidifier}
+                         co2={data.farm_data.co2} co2_val={data.farm_data.co2_val}>
+            </StatPreviewLarge>
         </div>
-        <div class="flex justify-center grow items-center white">
-            {#if humidifier_switch}
-                <Icon icon="mdi:air-humidifier" class="h-9 w-9 bg-teal-900 p-2 mr-1 rounded-lg"/>
-            {:else}
-                <Icon icon="mdi:air-humidifier" class="h-9 w-9 bg-teal-900 p-2 mr-1 rounded-lg"/>
-            {/if}
-            <input type="checkbox" class="toggle toggle-success" bind:checked={humidifier_switch} on:click={() => {toggle_switch("humidifier", data.farm_id, humidifier_switch)}}/>
+        <div class="flex justify-evenly w-full grow">
+            <div class="flex justify-center grow items-center white ">
+                {#if light_switch}
+                    <Icon icon="iconoir:light-bulb-on" class="h-9 w-9 bg-amber-500 p-2 mr-1 rounded-lg"/>
+                {:else}
+                    <Icon icon="iconoir:light-bulb-off" class="h-9 w-9 bg-amber-500 p-2 mr-1 rounded-lg"/>
+                {/if}
+                <input type="checkbox" class="toggle toggle-success" bind:checked={light_switch} on:click={() => {toggle_switch("light", data.farm_id, light_switch)}}/>
+            </div>
+            <div class="flex justify-center grow items-center white">
+                {#if ac_switch}
+                    <Icon icon="tabler:air-conditioning" class="h-9 w-9 bg-blue-900 p-2 mr-1 rounded-lg"/>
+                {:else}
+                    <Icon icon="tabler:air-conditioning-disabled" class="h-9 w-9 bg-blue-900 p-2 mr-1 rounded-lg"/>
+                {/if}
+                <input type="checkbox" class="toggle toggle-success" bind:checked={ac_switch} on:click={() => {toggle_switch("ac", data.farm_id, ac_switch)}}/>
+            </div>
+            <div class="flex justify-center grow items-center white">
+                {#if humidifier_switch}
+                    <Icon icon="mdi:air-humidifier" class="h-9 w-9 bg-teal-900 p-2 mr-1 rounded-lg"/>
+                {:else}
+                    <Icon icon="mdi:air-humidifier" class="h-9 w-9 bg-teal-900 p-2 mr-1 rounded-lg"/>
+                {/if}
+                <input type="checkbox" class="toggle toggle-success" bind:checked={humidifier_switch} on:click={() => {toggle_switch("humidifier", data.farm_id, humidifier_switch)}}/>
+            </div>
         </div>
-    </div>
     </div>
     <div class="flex w-full flex-col md:flex-row md:flex-wrap grow md:pl-10 md:pr-10">
         <div class="flex flex-col grow">
