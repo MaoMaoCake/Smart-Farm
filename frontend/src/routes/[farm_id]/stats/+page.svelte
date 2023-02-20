58fe2695cd8b7b27f3cf4f4c56f4e84e37e9f5a3
maomao <tangpansak@gmail.com>
2023-02-20 10:01:41 +0700

    initial carbon charts setup

--- frontend/src/routes/[farm_id]/stats/+page.svelte
+++ frontend/src/routes/[farm_id]/stats/+page.svelte
@@ -1,7 +1,27 @@
 <script lang="ts">
   import type { PageData } from './$types';
+  import "@carbon/styles/css/styles.css";
+  import "@carbon/charts/styles.css";
+  import { BarChartSimple } from "@carbon/charts-svelte";
 
   export let data: PageData;
 </script>
 
 <p>This is the stats for {data.farm_id}</p>
+<BarChartSimple
+        data={[
+    { group: "Qty", value: 65000 },
+    { group: "More", value: 29123 },
+    { group: "Sold", value: 35213 },
+    { group: "Restocking", value: 51213 },
+    { group: "Misc", value: 16932 },
+  ]}
+        options={{
+    title: "Simple bar (discrete)",
+    height: "400px",
+    axes: {
+      left: { mapsTo: "value" },
+      bottom: { mapsTo: "group", scaleType: "labels" },
+    },
+  }}
+/>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,12 @@ import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
 	plugins: [sveltekit()],
+	ssr: {
+		noExternal:
+			process.env.NODE_ENV === 'production'
+				? ['@carbon/charts', 'carbon-components']
+				: [],
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
