1eeebc3502aa5a95af5a826d633c772b9d2fb0f9
maomaocake <tangpansak@gmail.com>
2023-02-22 16:41:32 +0700

    placeholder div for tooltips

--- frontend/src/routes/[farm_id]/settings/HumiditySetting.svelte
+++ frontend/src/routes/[farm_id]/settings/HumiditySetting.svelte
@@ -2,18 +2,25 @@
     import Icon from "@iconify/svelte";
     import {FarmSettings} from "$lib/SettingStores";
 
-    function info(type: string){
-        alert(type)
-    }
+    let tooltip = false;
 </script>
 <div class="flex mt-10 justify-start w-full flex-col md: flex-row">
-    <div class="flex items-center w-full">
-        <div on:click={() => {info("light")}}>
+    <div class="flex items-center w-full relative">
+        <div on:click={() => {tooltip = true}} on:mouseenter={() => {tooltip = true}} on:mouseleave={() => {tooltip = false}}>
             <Icon icon="mdi:information" class="h-5 w-5 ml-5" />
         </div>
         <p class="bg-teal-900 rounded min-w-fit ml-5 pl-5 pr-5 white">Humidity Setting</p>
         <div class="divider w-full ml-2"></div>
-
+        {#if tooltip}
+            <div class="absolute left-10 top-10">
+                <div class="bg-yellow-200 w-56 h-52 rounded-xl relative z-30">
+                    <div class="text-black">
+                        <p>This is a button</p>
+                        <p>this button is very very cool</p>
+                    </div>
+                </div>
+            </div>
+        {/if}
     </div>
     <div class="flex justify-center items-center">
         <p class="ml-5 mr-5">Maximum Humidity Level</p>
--- frontend/src/routes/[farm_id]/settings/WateringSetting.svelte
+++ frontend/src/routes/[farm_id]/settings/WateringSetting.svelte
@@ -8,20 +8,29 @@
     $FarmSettings.watering_schedule = [{time_start: "09:50"},
         {time_start: "12:00"}]
 
-    function info(type: string){
-        alert(type)
-    }
     function addTime(){
         $FarmSettings.watering_schedule = [...$FarmSettings.watering_schedule, {time_start: "00:00"}]
     }
+
+    let tooltip = false;
 </script>
 <div class="flex mt-10 justify-start w-full flex-col md: flex-row">
-    <div class="flex items-center w-full">
-        <div on:click={() => {info("light")}}>
+    <div class="flex items-center w-full relative">
+        <div on:click={() => {tooltip = true}} on:mouseenter={() => {tooltip = true}} on:mouseleave={() => {tooltip = false}}>
             <Icon icon="mdi:information" class="h-5 w-5 ml-5" />
         </div>
         <p class="bg-blue-400 rounded min-w-fit ml-5 pl-5 pr-5 white">Watering Setting</p>
         <div class="divider w-full ml-2"></div>
+        {#if tooltip}
+            <div class="absolute left-10 top-10">
+                <div class="bg-yellow-200 w-56 h-52 rounded-xl relative z-30">
+                    <div class="text-black">
+                        <p>This is a button</p>
+                        <p>this button is very very cool</p>
+                    </div>
+                </div>
+            </div>
+        {/if}
     </div>
     <div class="flex flex-col pl-5">
         {#each $FarmSettings.watering_schedule as water, i}
