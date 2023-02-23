e37df6da452e9a42351c72a06b09a4e633d543b4
maomaocake <tangpansak@gmail.com>
2023-02-23 13:37:55 +0700

    ac card responsive

--- frontend/src/routes/[farm_id]/ac_list/ACCard.svelte
+++ frontend/src/routes/[farm_id]/ac_list/ACCard.svelte
@@ -2,6 +2,7 @@
     export let name;
     export let temp;
     export let status;
+    import Icon from '@iconify/svelte';
 </script>
 <div class="w-96 h-52 bg-gray-300 rounded-xl m-5 flex items-center justify-center">
     <div class="flex flex-col grow h-52">
@@ -9,14 +10,27 @@
             <p>{name}</p>
         </div>
         <div class="flex grow min-w-max h-5/6 justify-evenly items-center">
-            <div class="flex aspect-square bg-blue-900 h-24 w-24 white">
-                <p>{status}</p>
+            <div class="flex aspect-square bg-blue-900 h-24 w-24 white flex-col justify-center items-center rounded-xl">
+                {#if status}
+                    <Icon icon="tabler:air-conditioning" class="h-10 w-10"/>
+                {:else}
+                    <Icon icon="tabler:air-conditioning-disabled" class="h-10 w-10"/>
+                {/if}
+                <p class="text-xl">
+                    {status ? 'On' : 'Off'}
+                </p>
             </div>
-            <div class="flex">
-                <p>ON</p>
-                <input type="checkbox" class="toggle" bind:checked={status} />
-                <p>OFF</p>
+            <div class="flex flex-col items-center">
+                <div class="flex p-2 min-w-fit">
+                    <p class="badge bg-blue-900 text-white text-2xl p-4">{temp}°</p>
+                </div>
+                <div class="flex p-2">
+                    <p>ON</p>
+                    <input type="checkbox" class="toggle" bind:checked={status} />
+                    <p>OFF</p>
+                </div>
             </div>
+
         </div>
     </div>
 </div>
