80f483965992a900c45367fb6fc0031a9a031de2
maomaocake <tangpansak@gmail.com>
2023-03-03 10:11:07 +0700

    initial

--- frontend/src/routes/[farm_id]/light_preset/edit/[preset_id]/+page.svelte
+++ frontend/src/routes/[farm_id]/light_preset/edit/[preset_id]/+page.svelte
@@ -1,5 +1,29 @@
 <script lang="ts">
     import type {PageData} from "./$types";
+    import LightValueSmall from "./LightValueSmall.svelte";
     export let data: PageData;
+    let natural;
+    let uv;
+    let ir;
+
 </script>
-Edit page {data.preset_id}
+<div class="flex grow flex-col justify-center items-center md:flex-row">
+    <div class="flex md:hidden">
+        <LightValueSmall name={"light 1"} natural={23} uv={10} ir={12}/>
+    </div>
+    <div class="flex flex-col grow">
+        <div class="flex grow">Light Automation</div>
+        <div class="flex flex-col grow">
+            <div class="flex">
+                <p>Natural Light</p>
+                <input type="text" bind:value={natural} class="input input-bordered w-full max-w-xs" />
+            </div>
+            <div class="flex flex-col">
+                <input type="range" min="0" max="100" bind:value={natural} class="range" step="10" />
+            </div>
+        </div>
+        <div class="flex"></div>
+        <div class="flex"></div>
+        <div class="flex"></div>
+    </div>
+</div>
